08ecbb213061d1a2d0b820bdedabcfb14ced6594
James Coulson <james@coulson.ie>
2018-12-11 08:50:05 +0000

    Fix sed in install.sh

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 echo "LC_MONETARY=en_IE.UTF-8" >> /mnt/etc/locale.conf
 ln -sf /usr/share/zoneinfo/Europe/Dublin /mnt/etc/localtime
 chmod 600 /mnt/boot/initramfs-linux*
-sed -i "s/#PART_BOOT#/${part_boot}/g" /mnt/etc/default/grub
+sed -i "s|#PART_BOOT#|${part_boot}|g" /mnt/etc/default/grub
 
 arch-chroot /mnt useradd -mU -s /usr/bin/zsh -G wheel,uucp,video,audio,storage,games,input "$user"
 arch-chroot /mnt chsh -s /usr/bin/zsh
