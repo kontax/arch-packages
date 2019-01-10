5ef95080cc0dc33dc2a0a29b1d62e769bc7cf97d
James Coulson <james@coulson.ie>
2019-01-10 15:10:16 +0000

    Replace grub-btrfs config file in PKGBULID
    The grub-btrfs config file was named config, however this has been
    renamed as it's dealt with in the install script anyway. This makes
    the copy-files script easier to use as there are no special cases
    to take care of.

--- pkg/copyfiles.sh
+++ pkg/copyfiles.sh
@@ -7,12 +7,6 @@ mkdir build
 # Copy the AUR files to it
 cp PKGBUILD couldinho-base.install couldinho-desktop.install -t build
 
-# Copy grub-btrfs manually as it's named config which is too generic
-cp ../etc/default/grub-btrfs/config build/grub-btrfs-config
-
 # Copy the rest of the files over, without their directory structure
 find ../etc -type f -exec cp -t build/ {} +
-
-# Remove the grub-btrfs config file
-rm build/config
 
