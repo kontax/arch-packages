d5d212750450e39fdb8854a34b92da0ae4beec81
James Coulson <james@coulson.ie>
2019-02-08 14:00:48 +0000

    Remove vconsole.conf from install script

--- install.sh
+++ install.sh
@@ -248,7 +248,6 @@ echo "  [*] Generating base config files"
 mkdir /mnt/var/cache/pacman/couldinho-arch-aur
 genfstab -t PARTUUID /mnt >> /mnt/etc/fstab
 echo "${hostname}" > /mnt/etc/hostname
-echo "FONT=ter-112n" > /mnt/etc/vconsole.conf
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 echo "en_IE.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
