f5d78b0e4e590ec1573546c02e0273ea6979ae3e
James Coulson <github@coulson.ie>
2021-09-14 22:39:59 +0100

    Use UEFI secure boot instead of grub

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # WARNING: this script will destroy data on the selected disk.
 # This script can be run by executing the following:
-#   curl -sL https://git.io/coul-install | bash
+#   bash <(curl -sL https://git.io/coul-install)
 
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
@@ -120,6 +120,15 @@ function get_choice {
          "${options[@]}"
  }
 
+echo ""
+echo "#####"
+echo "# Checking UEFI boot mode"
+echo "##"
+if [ ! -f /sys/firmware/efi/fw_platform_size ]; then
+    echo >&2 "You must boot in UEFI mode to continue"
+    exit 2
+fi
+
 
 echo ""
 echo "#####"
@@ -153,6 +162,8 @@ devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac | 
 read -r -a devicelist <<< $devicelist
 device=$(get_choice "Installation" "Select installation disk" "${devicelist[@]}") || exit 1
 clear
+
+luks_header_device=$(get_choice "Installation" "Select disk for LUKS header" "${devicelist[@]}") || exit 1
 
 
 config=$(get_input "Config" "Enter location of config files (leave blank if required)") || exit 1
@@ -194,60 +205,63 @@ echo "#####"
 echo "# Setting up partitions"
 echo "##"
 
-swap_size=$(free --mebi | awk '/Mem:/ {print $2}')
-swap_end=$(( $swap_size + 129 + 1 ))MiB
-bios=$(if [ -f /sys/firmware/efi/fw_platform_size ]; then echo "gpt"; else echo "msdos"; fi)
-part=$(if [[ $bios == "gpt" ]]; then echo "ESP"; else echo "primary"; fi)
+# Delete any previous attempts at installation
+umount -R /mnt 2>/dev/null || true
+cryptsetup luksClose luks 2> /dev/null || true
 
-parted --script "${device}" -- mklabel ${bios} \
-  mkpart ${part} fat32 1Mib 129MiB \
-  set 1 boot on \
-  mkpart primary linux-swap 129MiB ${swap_end} \
-  mkpart primary ${swap_end} 100%
+lsblk -plnx size -o name "${device}" | xargs -n1 wipefs --all
+sgdisk --clear "${device}" \
+    --new 1::-511MiB "${device}" \
+    --new 2::0 --typecode 2:ef00 "${device}"
+sgdisk --change-name=1:primary --change-name=2:ESP "${device}"
 
-# Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1
-# but not /dev/mmcblk0boot1 while being able to match /dev/sda1 on other devices.
-part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
-part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"
-part_root="$(ls ${device}* | grep -E "^${device}p?3$")"
+part_root="$(ls ${device}* | grep -E "^${device}p?1$")"
+part_boot="$(ls ${device}* | grep -E "^${device}p?2$")"
 
-wipefs "${part_boot}"
-wipefs "${part_swap}"
-wipefs "${part_root}"
+if [ "$device" != "$luks_header_device" ]; then
+    cryptargs="--header $luks_header_device"
+else
+    cryptargs=""
+    luks_header_device="$part_root"
+fi
+
+
+echo -e "\n  [*] Formatting partitions"
 
 mkfs.vfat -n "EFI" -F32 "${part_boot}"
-mkswap "${part_swap}"
-echo -n ${passphrase} | cryptsetup luksFormat --type luks1 "${part_root}"
-echo -n ${passphrase} | cryptsetup luksOpen "${part_root}" luks
+echo -n ${passphrase} | cryptsetup luksFormat \
+    --type luks2 \
+    --pbkdf argon2id \
+    --label luks \
+    $cryptargs "${part_root}"
+echo -n ${passphrase} | cryptsetup luksOpen $cryptargs "${part_root}" luks
 mkfs.btrfs -L btrfs /dev/mapper/luks
 
 swapon "${part_swap}"
 
-echo "  [*] Setting up BTRFS subvolumes"
+echo -e "\n  [*] Setting up BTRFS subvolumes"
 mount /dev/mapper/luks /mnt
 btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/pkgs
+btrfs subvolume create /mnt/docker
 btrfs subvolume create /mnt/logs
 btrfs subvolume create /mnt/tmp
+btrfs subvolume create /mnt/swap
 btrfs subvolume create /mnt/snapshots
 umount /mnt
 
-mount -o noatime,nodiratime,discard,compress=lzo,subvol=root /dev/mapper/luks /mnt
-mkdir -p /mnt/{mnt/btrfs-root,boot/efi,home,var/{cache/pacman,log,tmp},.snapshots}
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=root /dev/mapper/luks /mnt
+mkdir -p /mnt/{mnt/btrfs-root,boot/efi,home,var/{cache/pacman,log,tmp,lib/docker},swap,.snapshots}
 mount "${part_boot}" /mnt/boot/efi
-mount -o noatime,nodiratime,discard,compress=lzo,subvol=/ /dev/mapper/luks /mnt/mnt/btrfs-root
-mount -o noatime,nodiratime,discard,compress=lzo,subvol=home /dev/mapper/luks /mnt/home
-mount -o noatime,nodiratime,discard,compress=lzo,subvol=pkgs /dev/mapper/luks /mnt/var/cache/pacman
-mount -o noatime,nodiratime,discard,compress=lzo,subvol=logs /dev/mapper/luks /mnt/var/log
-mount -o noatime,nodiratime,discard,compress=lzo,subvol=tmp /dev/mapper/luks /mnt/var/tmp
-mount -o noatime,nodiratime,discard,compress=lzo,subvol=snapshots /dev/mapper/luks /mnt/.snapshots
-
-
-echo "  [*] Creating an encrypted key for booting"
-dd bs=512 count=4 if=/dev/urandom of=/mnt/crypto_keyfile.bin
-chmod 000 /mnt/crypto_keyfile.bin
-echo -n ${passphrase} | cryptsetup luksAddKey ${part_root} /mnt/crypto_keyfile.bin
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=/ /dev/mapper/luks /mnt/mnt/btrfs-root
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=home /dev/mapper/luks /mnt/home
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=pkgs /dev/mapper/luks /mnt/var/cache/pacman
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=docker /dev/mapper/luks /mnt/var/lib/docker
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=logs /dev/mapper/luks /mnt/var/log
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=tmp /dev/mapper/luks /mnt/var/tmp
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=swap /dev/mapper/luks /mnt/swap
+mount -o noatime,nodiratime,discard,compress=zstd,subvol=snapshots /dev/mapper/luks /mnt/.snapshots
 
 
 echo "#####"
@@ -257,8 +271,7 @@ echo "##"
 
 if [[ ! -a /etc/pacman.d/couldinho-arch-aur ]]; then
 
-    echo ""
-    echo "  [*] Configuring remote AUR details"
+    echo -e "\n  [*] Configuring remote AUR details"
 
     cat >>/etc/pacman.d/couldinho-arch-aur <<EOF
 [options]
@@ -275,11 +288,17 @@ Include = /etc/pacman.d/couldinho-arch-aur
 EOF
 fi
 
-echo "  [*] Installing packages"
+echo -e "\n  [*] Installing packages"
 pacstrap /mnt $system
 
+echo -e "\n  [*] Creating an encrypted key for booting"
+cryptsetup luksHeaderBackup "${luks_header_device}" --header-backup-file /tmp/header.img
+luks_header_size="$(stat -c '%s' /tmp/header.img)"
+rm -f /tmp/header.img
+echo "cryptdevice=PARTLABEL=primary:luks:allow-discards cryptheader=LABEL=luks:0:$luks_header_size root=LABEL=btrfs rw rootflags=subvol=root quiet mem_sleep_default=deep" > /mnt/etc/kernel/cmdline
 
-echo "  [*] Generating base config files"
+
+echo -e "\n  [*] Generating base config files"
 mkdir /mnt/var/cache/pacman/couldinho-arch-aur
 genfstab -t PARTUUID /mnt >> /mnt/etc/fstab
 echo "${hostname}" > /mnt/etc/hostname
@@ -288,14 +307,26 @@ echo "en_IE.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 echo "LC_MONETARY=en_IE.UTF-8" >> /mnt/etc/locale.conf
 ln -sf /usr/share/zoneinfo/Europe/Dublin /mnt/etc/localtime
-chmod 600 /mnt/boot/initramfs-linux*
-sed -i "s|#PART_ROOT#|${part_root}|g" /mnt/etc/default/grub
-
 arch-chroot /mnt locale-gen
 
-echo "  [*] Installing grub"
-arch-chroot /mnt grub-install ${device}
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+cat << EOF > /mnt/etc/mkinitcpio.conf
+MODULES=()
+BINARIES=()
+FILES=()
+HOOKS=(base consolefont udev autodetect modconf block encrypt-dh filesystems keyboard)
+EOF
+arch-chroot /mnt mkinitcpio -p linux
+arch-chroot /mnt arch-secure-boot initial-setup
+
+echo -e "\n  [*] Configuring swap file"
+swap_size=$(free --mebi | awk '/Mem:/ {print $2}')
+truncate -s 0 /mnt/swap/swapfile
+chattr +C /mnt/swap/swapfile
+btrfs property set /mnt/swap/swapfile compression none
+dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=${swap_size}
+chmod 600 /mnt/swap/swapfile
+mkswap /mnt/swap/swapfile
+echo "/swap/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
 
 
 echo "#####"
@@ -303,7 +334,7 @@ echo "# Setting up user account and home folder,"
 echo "# including the dotfile repo."
 echo "##"
 
-echo "  [*] Creating user and shell"
+echo -e "\n  [*] Creating user and shell"
 arch-chroot /mnt useradd -m -s /usr/bin/zsh -g users -G wheel,uucp,video,audio,storage,games,input "$user"
 arch-chroot /mnt chsh -s /usr/bin/zsh
 
@@ -311,7 +342,7 @@ echo "$user:$password" | chpasswd --root /mnt
 echo "root:$password" | chpasswd --root /mnt
 
 if [ ! -z $CONF_FILE_LOCATION ]; then
-    echo "  [*] Cloning dotfiles to home folder"
+    echo -e "\n  [*] Cloning dotfiles to home folder"
     arch-chroot /mnt sudo -u $user bash < <( \
         curl -skL $CONF_FILE_LOCATION \
         | openssl aes-256-cbc -salt -d -k "$CONF_FILE_PASS")
@@ -320,5 +351,5 @@ fi
 # Finish off installing zsh
 arch-chroot /mnt sudo -u "$user" zsh -ic true
 
-echo "[*] DONE - Install setup from $HOME/dotfiles"
+echo "\n[*] DONE - Install setup from $HOME/dotfiles"
 
