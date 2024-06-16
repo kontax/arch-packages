ad9d516a9b55b681f55c7cb735c466093edad225
James Coulson <git@coulson.ie>
2024-06-16 11:34:48 +0100

    Re-add old build script

---
+++ pkg/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+GIT_ROOT=$(git -C $DIR rev-parse --show-toplevel)
+CONF_ARCHIVE="$GIT_ROOT/pkg/conf-files.tar.xz"
+
+# Package config files
+if [ -f $CONF_ARCHIVE ]; then
+    rm $CONF_ARCHIVE
+fi
+tar -cf $CONF_ARCHIVE -C $GIT_ROOT/conf/ .
+
+# Set checksums
+CHKSUM=$(sha256sum $CONF_ARCHIVE | awk '{ print $1 }')
+sed -i "s/sha256sums.*/sha256sums=($CHKSUM)/g" "$GIT_ROOT/pkg/PKGBUILD"
+
+# Build the package
+if [ -d build ]; then
+    rm -r build
+fi
+mkdir build
+cp *.install PKGBUILD conf-files.tar.xz -t build
+cd build
